ad0fe949abbb1c6d70bb3cd09c44e886a0c6ac80
irscript <mindowos@163.com>
2024-08-24 09:43:06 +0800

    颜色函数

--- airkit/GUI/UI/UIColor.hpp
+++ airkit/GUI/UI/UIColor.hpp
@@ -8,12 +8,24 @@ namespace airkit
     {
         RGBA(uint8_t r = 0, uint8_t g = 0, uint8_t b = 0, uint8_t a = 0)
             : mRGBA(r | (g << 8) | (b << 16) | (a << 24)) {}
-        RGBA(uint32_t rgba) : mRGBA(rgba) {}
-        RGBA(int32_t rgba) : mRGBA(rgba) {}
-        RGBA(const RGBA &rgba) : mRGBA(rgba.mRGBA) {}
-        RGBA &operator=(uint32_t rgba)
+
+        // 0xRRGGBB
+        RGBA(uint32_t rgb)
         {
-            mRGBA = rgba;
+            mR = (rgb >> 16) & 0xFF;
+            mG = (rgb >> 8) & 0xFF;
+            mB = rgb & 0xFF;
+            mA = 1;
+        }
+        RGBA(const RGBA &rgba) : mRGBA(rgba.mRGBA) {}
+
+        // 0xRRGGBB
+        RGBA &operator=(uint32_t rgb)
+        {
+            mR = (rgb >> 16) & 0xFF;
+            mG = (rgb >> 8) & 0xFF;
+            mB = rgb & 0xFF;
+            mA = 1;
             return *this;
         }
         RGBA &operator=(const RGBA &rgba)
@@ -34,12 +46,34 @@ namespace airkit
             };
         };
 
-        float toFlt() const { return *(float *)&mRGBA; }
-
-        static float fromRGBA(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+        // 0xRRGGBB
+        uint32_t toRGB() const
         {
-            RGBA clr(r, g, b, a);
-            return *(float *)&clr.mRGBA;
+            uint32_t hex = mB | (mG << 8) | (mR << 16);
+            return hex;
+        }
+        // 0xRRGGBBAA
+        uint32_t toRGBA() const
+        {
+            uint32_t hex = mA | (mB << 8) | (mG << 16) | (mR << 24);
+            return hex;
+        }
+        // 0xRRGGBBAA
+        static RGBA fromRGBA(uint32_t rgba)
+        {
+            uint8_t r = (rgba >> 24) & 0xFF;
+            uint8_t g = (rgba >> 16) & 0xFF;
+            uint8_t b = (rgba >> 8) & 0xFF;
+            uint8_t a = rgba & 0xFF;
+            return RGBA(r, g, b, a);
+        }
+        // 0xRRGGBB
+        static RGBA fromRGB(uint32_t rgb)
+        {
+            uint8_t r = (rgb >> 16) & 0xFF;
+            uint8_t g = (rgb >> 8) & 0xFF;
+            uint8_t b = rgb & 0xFF;
+            return RGBA(r, g, b, 1);
         }
     };
 }
